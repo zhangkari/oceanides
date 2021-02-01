a3c8b08e027d9a27d141b342a713b260e39848c5
Karic <zhangkaric@gmail.com>
2021-02-01 16:00:40 +0800

    Release libOceanides v0.0.1

--- app/src/main/java/com/class100/yunshixun/DevOpsActivity.kt
+++ app/src/main/java/com/class100/yunshixun/DevOpsActivity.kt
@@ -2,14 +2,9 @@ package com.class100.yunshixun
 
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import com.class100.atropos.env.context.AtPrefs
-import com.class100.hades.http.HaApiCallback
-import com.class100.hades.http.HaHttpClient
 import com.class100.oceanides.OcActivity
 import com.class100.oceanides.camera.OcCameraActivity
-import com.class100.poseidon.PsWebActivity
-import com.class100.yunshixun.model.request.YsxTokenRequest
 import kotlinx.android.synthetic.main.activity_devops.*
 
 class DevOpsActivity : OcActivity() {
@@ -30,8 +25,6 @@ class DevOpsActivity : OcActivity() {
         val data = AtPrefs.get(DEV_OPS_URL_HISTORY, "");
         et_url.setText(data)
         btn_go.setOnClickListener {
-            PsWebActivity.launch(it.context, et_url.text.toString())
-            AtPrefs.put(DEV_OPS_URL_HISTORY, et_url.text.toString())
         }
 
         btn_camera.setOnClickListener {
@@ -39,17 +32,7 @@ class DevOpsActivity : OcActivity() {
         }
 
         btn_get_token.setOnClickListener {
-            HaHttpClient.getInstance().enqueue(
-                YsxTokenRequest("15928695284", "ZSC1988love"),
-                object : HaApiCallback<String> {
-                    override fun onError(code: Int, message: String?) {
-                        Log.d(TAG, "error:$code, message")
-                    }
 
-                    override fun onSuccess(content: String?) {
-                        Log.d(TAG, "ok:$content")
-                    }
-                })
         }
     }
 }
--- poseidon/src/main/java/com/class100/poseidon/extension/WebExtPlugin.java
+++
@@ -1,9 +0,0 @@
-package com.class100.poseidon.extension;
-
-import com.class100.atropos.env.AtPlugin;
-
-public abstract class WebExtPlugin extends AtPlugin<WebExtPluginEnv> {
-    public WebExtPlugin(WebExtPluginEnv webExtPluginEnv) {
-        super(webExtPluginEnv);
-    }
-}
